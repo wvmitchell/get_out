1d0d38305a1610879b4395eda54b65a3b2948329
William Mitchell <wvmitchell@gmail.com>
2013-06-02 11:14:29 -0600

    email working

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,14 @@
+# specify mail
+ActionMailer::Base.smtp_settings = {
+  :user_name => "wvm2009",
+  :password => "Wi11iam!",
+  :domain => "get-out.herokuapp.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
